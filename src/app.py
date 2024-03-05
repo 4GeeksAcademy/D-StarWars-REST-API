56bd2501226f8fda589250ade68577fe4ebe3719
Dana M <147849932+dkfmedia@users.noreply.github.com>
2024-03-05 01:59:57 +0000

    errors on get planets looks like the issue is on module

--- src/app.py
+++ src/app.py
@@ -1,6 +1,3 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
 import os
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
@@ -8,7 +5,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Planets
 #from models import Person
 
 app = Flask(__name__)
@@ -32,20 +29,72 @@ def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
-@app.route('/')
+@app.route('/')git 
 def sitemap():
     return generate_sitemap(app)
 
+@app.route('/people/<int:people_id>', methods=['GET'])
+def handle_get_one_person(people_id):
+    person = People.query.get(people_id)
+    if person:
+        return jsonify({
+            "id": person.id,
+            "name": person.name,
+            "description": person.description
+        }), 200
+    else:
+        return jsonify({"error": "Person not found"}), 404
+
+@app.route('/people', methods=['GET'])
+def handle_get_all_people():
+    people = People.query.all()
+    return jsonify([{
+        "id": person.id,
+        "name": person.name,
+        "description": person.description
+    } for person in people]), 200
+
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def handle_get_one_planet(planet_id):
+    planet = Planets.query.get(planet_id)
+    if planet:
+        return jsonify({
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description
+        }), 200
+    else:
+        return jsonify({"error": "Planet not found"}), 404
+
+@app.route('/planets', methods=['GET'])
+def handle_get_all_planets():
+    planets = Planets.query.all()
+    return jsonify([{
+        "id": planet.id,
+        "name": planet.name,
+        "description": planet.description
+    } for planet in planets]), 200
+
+
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_users():
+    users = User.query.all()
+    return jsonify([{
+        "id": user.id,
+        "name": user.name,
+    } for user in users]), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
-
-    return jsonify(response_body), 200
-
-# this only runs if `$ python src/app.py` is executed
-if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=False)
+@app.route('/user/favorites', methods=['GET'])
+def get_user_favorites():
+    user = User.query.get()
+    if user:
+        favorites = user.favorites
+        return jsonify([{
+            "id": favorite.id,
+            "name": favorite.name,
+            "planet": favorite.planet,
+            "person": favorite.person  
+        } for favorite in favorites]), 200
+    else:
+        return jsonify({"error": "User not found"}), 404
--- src/models.py
+++ src/models.py
@@ -1,12 +1,15 @@
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorites = db.relationship("Favorites", back_populates= "user")
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -17,3 +20,49 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+class Favorites(db.Model): 
+    __tablename__ = "favorites"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship("User", back_populates="favorites")
+    people = db.relationship("User", back_populates="favorites")
+    planet = db.relationship("User", back_populates="favorites")
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "people": self.people,
+            "planet":self.planet
+        }
+
+
+class People(db.Model): 
+    __tablename__ = "people"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable = False)
+    description = db.Column(db.String(200), nullable=False)
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description
+        }
+    
+class Planets(db.Model): 
+    __tablename__ = "planets"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable = False)
+    description = db.Column(db.String(200), nullable=False)
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description
+        }
+    
+    
